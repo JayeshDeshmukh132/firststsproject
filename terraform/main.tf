a016978bde94ad476f5db1170ebaca42229a74fc
JayeshDeshmukh132 <88625075+JayeshDeshmukh132@users.noreply.github.com>
2025-11-22 15:03:45 +0530

    Update AMI ID for EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0a716d3f3b16d290c" 
+  ami           = "ami-0fa91bc90632c73c9" 
   instance_type = "t3.micro"
   availability_zone = "eu-north-1a"
   key_name      = "ipat-eunorth1"
