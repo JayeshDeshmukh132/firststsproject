2f799eba4de9f571116524b32a42f325390ce20e
JayeshDeshmukh132 <88625075+JayeshDeshmukh132@users.noreply.github.com>
2025-11-22 15:16:24 +0530

    Update key_name for AWS EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "my_ec2" {
   ami           = "ami-0fa91bc90632c73c9" 
   instance_type = "t3.micro"
   availability_zone = "eu-north-1a"
-  key_name      = "ipat-eunorth1"
+  key_name      = "devops-project-swabhav"
 
   tags = {
     Name = "firststsproject"
